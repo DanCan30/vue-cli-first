4d2740c2a29bff688e406903c204300840f91942
DanCan30 <danielecan3008@gmail.com>
2022-07-14 14:45:22 +0200

    Title component done

---
+++ src/components/title.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>
+        My first APP with Vue CLI!!
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "MainTitle",
+}
+</script>
+
+<style>
+
+</style>
